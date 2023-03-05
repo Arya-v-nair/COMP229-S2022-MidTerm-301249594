22649af4279094e125c9af9517ac6e2808b1dabd
Arya-v-nair <aryavnair79@gmail.com>
2023-03-04 19:25:47 -0500

    Webapp midterm code commit

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI" :"mongodb+srv://aryavnair:aryavnair@cluster1.etif0nk.mongodb.net/books?retryWrites=true&w=majority"
+};
---
+++ server/routes/books.js
@@ -0,0 +1,39 @@
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the book model
+let book = require('../models/books');
+
+let bookController = require('../controllers/books');
+
+/* GET books List page. READ */
+router.get('/book-list', bookController.displayBookList);
+
+//  GET the Book Details page in order to add a new Book
+router.get('/add', bookController.displayAddPage);
+
+// POST process the Book Details page and create a new Book - CREATE
+
+router.post('/add', bookController.processAddPage);
+
+
+// GET the Book Details page in order to edit an existing Book
+
+router.get('/:id', bookController.displayEditPage);
+
+
+// POST - process the information passed from the details form and update the document
+
+router.post('/:id', bookController.processEditPage);
+
+
+
+// GET - process the delete by user id
+
+router.get('/delete/:id', bookController.performDelete);
+
+
+
+module.exports = router;
